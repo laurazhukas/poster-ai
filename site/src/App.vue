0428c680dfae98fef52c34a7e9d19de71788eb80
Robert M. Nill <rmnill202mail@gmail.com>
2019-02-16 14:08:28 -0500

    Added vuetify boilerplate, added vue-upload-component

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,29 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <v-app>
+        <!-- Navigation bar -->
+    <v-toolbar dense flat>
+      <v-toolbar-title>Poster-AI</v-toolbar-title>
+      <v-spacer/>
+      <v-toolbar-side-icon class="hidden-sm-and-up" @click="drawerOpen = !drawerOpen"/>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn to="/" flat>Home</v-btn>
+        <v-btn to="/updates" flat>Updates</v-btn>
+        <v-btn to="/about" flat>About</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <!-- Drawer for mobile menu -->
+    <v-navigation-drawer v-model="drawerOpen" absolute temporary right>
+      <v-list>
+        <!-- Home Link -->
+        <v-list-tile to="/">
+          <v-list-tile-action> <v-icon>home</v-icon> </v-list-tile-action>
+          <v-list-tile-title>Home</v-list-tile-title>
+        </v-list-tile>
+
+        <!-- Updates Link -->
+        <v-list-tile to="/updates">
+          <v-list-tile-action> <v-icon>description</v-icon> </v-list-tile-action>
+          <v-list-tile-title>Updates</v-list-tile-title>
+        </v-list-tile>
+
+        <!-- About Link -->
+        <v-list-tile to="/about">
+          <v-list-tile-action> <v-icon>info</v-icon> </v-list-tile-action>
+          <v-list-tile-title>About</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- And finally all the way at the bottom... the actual page content :) -->
     <router-view/>
-  </div>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      drawerOpen: false
     }
   }
 }
-</style>
+</script>
--- site/src/main.js
+++ site/src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import './plugins/vuetify'
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import Vuetify from 'vuetify';
 
-Vue.config.productionTip = false
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
