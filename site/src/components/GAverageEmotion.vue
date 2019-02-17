3a8ead89461bf4049e954d7656a1c29fc535ef97
Robert M. Nill <rmnill202mail@gmail.com>
2019-02-17 01:29:32 -0500

    Start of graph work

---
+++ site/src/components/GAverageEmotion.vue
@@ -0,0 +1,44 @@
+<script>
+import { Bar } from 'vue-chartjs';
+
+export default {
+  extends: Bar,
+  props: ['data', 'emotion'],
+  mounted() {
+    this.renderChart(this.avgemotion, this.options);
+  },
+  computed: {
+    avgemotion() {
+      // Loop through each poster
+      
+        // For each poster, find the average for the given emotion
+
+
+      // Return the data structure that chartJS expects
+      return {
+        labels: ['A', 'B', 'C', 'D'],
+        datasets: [{
+          label: `Average Emotion - ${this.emotion}`,
+          backgroundColor: '#81a1d1',
+          data: [1, 2, 3, 4],
+        },]
+      }
+    }
+  },
+  data() {
+    return {
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              suggestedMin: 0,
+              suggestedMax: 5
+            }
+          }]
+        },
+        maintainAspectRatio: false
+      }
+    };
+  }
+}
+</script>
--- site/src/views/Results.vue
+++ site/src/views/Results.vue
@@ -1,15 +1,105 @@
 <template>
-  <div>
-    Results! {{$route.params.id}}
+  <div class="page-style">
+    <!-- Title -->
+    <div class="font-weight-medium display-3">Survey Results</div>
+
+    <!-- Display things in a grid -->
+    <div class="page-grid">
+      <!-- Raw numbers overview -->
+      <div class="numbers-overview">
+        <div class="font-weight-light headline">{{postersCount}} {{posterMsg}}</div>
+        <div class="font-weight-light headline">{{usersCount}} {{usersMsg}}</div>
+        <div class="font-weight-light headline"><span class="">{{targetMsg}}</span> : {{targetEmotion}}</div>
+      </div>
+
+
+      <div class="avg-emotions"> <!-- Average emotions graph -->
+        <avg-emot class="graph-style"/>
+        <v-combobox label="Select Emotions" class="graph-style"
+          v-model="chosenEmotions" :items="possibleEmotions"
+          multiple chips/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import AvgEmot from '../components/GAverageEmotion';
 
+export default {
+  components: { AvgEmot },
+  data() {
+    return {
+      emotions: {
+        'Anger': {key: 'anger', color: '#ffffff'},
+        'Contempt': {key: 'contempt', color: '#ffffff'},
+        'Disgust': {key: 'disgust', color: '#ffffff'},
+        'Fear': {key: 'fear', color: '#ffffff'},
+        'Happiness': {key: 'happiness', color: '#ffffff'},
+        'Neutral': {key: 'neutral', color: '#ffffff'},
+        'Sadness': {key: 'sadness', color: '#ffffff'},
+        'Surprise': {key: 'surprise', color: '#ffffff'},
+      },
+      chosenEmotions: [],
+      possibleEmotions: [
+        'Anger',
+        'Contempt',
+        'Disgust',
+        'Fear',
+        'Happiness',
+        'Neutral',
+        'Sadness',
+        'Surprise'
+      ],
+      postersCount: 5,
+      usersCount: 5,
+      targetEmotion: 'Happiness'
+    };
+  },
+  computed: {
+    posterMsg() {
+      return `Posters Evaluated`;
+    },
+    usersMsg() {
+      return `Users Provided Feedback`;
+    },
+    targetMsg() {
+      return `Target Emotion`;
+    }
+  }
 }
 </script>
 
 <style>
-
+.graph-style {
+  max-height: 400px;
+  max-width: 500px;
+  margin: auto;
+}
+.page-style {
+  text-align: center;
+  margin-left: 10%;
+  margin-right: 10%;
+}
+.page-grid {
+  display: grid;
+  grid-template-columns: 50% 50%;
+}
+.numbers-overview {
+  text-align: left;
+}
+@media only screen and (max-width: 729px) {
+  .page-style {
+    text-align: center;
+    margin-left: 20px;
+    margin-right: 20px;
+  }
+  .page-grid {
+    display: grid;
+    grid-template-columns: 100%;
+  }
+  .numbers-overview {
+    text-align: center;
+  }
+}
 </style>
